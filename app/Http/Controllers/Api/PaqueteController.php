260542c87b2a82e22815054970b04b44d0ef4e40
LAGS(Angel) <109058663+Xl4gsX@users.noreply.github.com>
2026-05-15 01:25:34 -0600

    feat: implement PaqueteController for CRUD operations and public menu listing of packages

--- app/Http/Controllers/Api/PaqueteController.php
+++ app/Http/Controllers/Api/PaqueteController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\OrdenDetalle;
 use App\Models\Paquete;
-use App\Models\Producto;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
